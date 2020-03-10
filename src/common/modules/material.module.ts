d5103895169ab28367355e413402f31c2f3b3d4e
constf1 <const.f1@yandex.ru>
2020-03-10 12:11:25 +0300

    responsive App

--- src/common/modules/material.module.ts
+++ src/common/modules/material.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatFormFieldModule,
+    MatIconModule,
+    MatListModule,
     MatSelectModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
     MatFormFieldModule,
+    MatIconModule,
+    MatListModule,
     MatSelectModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
