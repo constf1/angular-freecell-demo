e1576ddedaae0e5af203d12a11d4e7e686cb7b4c
constf1 <const.f1@yandex.ru>
2020-08-14 16:33:58 +0300

    proto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component, HostListener, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  ViewChild,
+  ElementRef,
+  OnInit,
+  AfterViewInit
+} from "@angular/core";
 
-import { randomIneger } from '../common/math-utils'
-import { Autoplay } from '../common/autoplay'
+// import { randomIneger } from "../common/math-utils";
+// import { Autoplay } from "../common/autoplay";
 
 interface Letter {
   value: string;
@@ -10,26 +17,45 @@ interface Letter {
   alternatives?: string[];
 }
 
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffle<T>(arr: Array<T>): Array<T> {
+  for (var i = arr.length; --i > 0; ) {
+    var j = Math.floor((i + 1) * Math.random());
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+}
+
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit, AfterViewInit  {
-  alternatives = [ 'а', 'о', 'э', 'и', 'у', 'ы', 'е', 'ю', 'я'];
+export class AppComponent implements OnInit, AfterViewInit {
+  alternatives = ["а", "о", "э", "и", "у", "ы", "е", "ю", "я"];
   selection = 1;
   // а, о, э, и, у, ы, е, ю, я
   // з…мл…ника => земляника
   letters: Letter[] = [
-    { value: 'з' },
-    { value: 'е', isHidden: true, alternatives: [ 'и', 'е', 'я', 'э', 'ы' ] },
-    { value: 'м' },
-    { value: 'л' },
-    { value: 'я', isHidden: true, alternatives: ['и', 'е', 'я'] },
-    { value: 'н' },
-    { value: 'и' },
-    { value: 'к' },
-    { value: 'а' },
+    { value: "з" },
+    {
+      value: "е",
+      isHidden: true,
+      alternatives: shuffle(["и", "е", "я", "э", "ы"])
+    },
+    { value: "м" },
+    { value: "л" },
+    {
+      value: "я",
+      isHidden: true,
+      alternatives: shuffle(["и", "е", "я", "э", "ы"])
+    },
+    { value: "н" },
+    { value: "и" },
+    { value: "к" },
+    { value: "а" }
   ];
 
   onAnswer(value: string) {
@@ -40,6 +66,15 @@ export class AppComponent implements OnInit, AfterViewInit  {
       this.selection = this.letters.findIndex(i => i.isHidden);
     }
   }
+
+  isLetterValid(index: number) {
+    const l = this.letters[index];
+    if (l) {
+      return !l.result || l.value === l.result;
+    }
+    return false;
+  }
+
   // @ViewChild('mainRef', {static: true}) mainRef: ElementRef<HTMLElement>;
   // @ViewChild(FreecellComponent, {static: false}) freecellComponent: FreecellComponent;
 
@@ -51,9 +86,11 @@ export class AppComponent implements OnInit, AfterViewInit  {
   //   }
   // }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  ngAfterViewInit() {
+  ngAfterViewInit() {}
+
+  trackByIndex(index: number) {
+    return index;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 import { FreecellComponent } from '../freecell/freecell.component';
 import { RatioKeeperComponent } from '../ratio-keeper/ratio-keeper.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FreecellComponent, RatioKeeperComponent ],
+  declarations: [ AppComponent, FreecellComponent, RatioKeeperComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
