f6b34d68ba3db53bfeadc4c7752a6ce0103484c2
constf1 <const.f1@yandex.ru>
2020-08-14 14:46:27 +0300

    prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { Component, HostListener, ViewChild, ElementRef, OnInit, AfterViewInit }
 import { randomIneger } from '../common/math-utils'
 import { Autoplay } from '../common/autoplay'
 
+interface Letter {
+  value: string;
+  isHidden?: boolean;
+}
 
 @Component({
   selector: 'my-app',
@@ -10,6 +14,25 @@ import { Autoplay } from '../common/autoplay'
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent implements OnInit, AfterViewInit  {
+  selection = 0;
+  letters: Letter[] = [
+    { value: 'а', isHidden: true },
+    { value: 'л' },
+    { value: 'ф' },
+    { value: 'а', isHidden: true },
+    { value: 'в' },
+    { value: 'и' },
+    { value: 'т' },
+  ];
+
+  onAnswer(value: string) {
+    const l = this.letters[this.selection];
+    if (l) {
+      l.value = value;
+      l.isHidden = false;
+      this.selection = this.letters.findIndex(i => i.isHidden);
+    }
+  }
   // @ViewChild('mainRef', {static: true}) mainRef: ElementRef<HTMLElement>;
   // @ViewChild(FreecellComponent, {static: false}) freecellComponent: FreecellComponent;
 
